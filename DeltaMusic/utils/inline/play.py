9fa76b16f0a96fe9a69b6859bb40f0511ab2d915
Kotak Biasa <muhnurfauzan73@gmail.com>
2025-02-02 03:31:08 +0800

    Update play.py

--- DeltaMusic/utils/inline/play.py
+++ DeltaMusic/utils/inline/play.py
@@ -3,7 +3,7 @@ import math
 from pyrogram.types import InlineKeyboardButton
 
 from DeltaStream.utils.formatters import time_to_seconds
-from DeltaStream import app
+from DeltaMusic import app
 
 def track_markup(_, videoid, user_id, channel, fplay):
     buttons = [
