dbdc8e24bc1a14ef0958b0f06738874cb1429bd9
Kotak Biasa <muhnurfauzan73@gmail.com>
2025-02-04 23:21:33 +0800

    g

--- DeltaMusic/utils/__init__.py
+++ DeltaMusic/utils/__init__.py
@@ -7,3 +7,14 @@ from .inline import *
 from .pastebin import *
 from .pyro_progress import *
 from .sys import *
+
+# Define pyro_cooldown if not already defined
+def pyro_cooldown():
+    # Implementation of pyro_cooldown
+    pass
+
+# Export pyro_cooldown
+__all__ = [
+    # ...existing exports...
+    'pyro_cooldown',
+]
