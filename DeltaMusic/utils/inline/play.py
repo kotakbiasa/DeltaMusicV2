1723ceb0ae0214c81ff3300e855d6855a0cdf7a0
Kotak Biasa <muhnurfauzan73@gmail.com>
2025-02-06 16:17:27 +0800

    Fix

--- DeltaMusic/utils/inline/play.py
+++ DeltaMusic/utils/inline/play.py
@@ -33,45 +33,45 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "❥—————————"
+        bar = "◉——————————"
     elif 10 < umm < 20:
-        bar = "—❥————————"
+        bar = "—◉—————————"
     elif 20 <= umm < 30:
-        bar = "——❥———————"
+        bar = "——◉————————"
     elif 30 <= umm < 40:
-        bar = "———❥——————"
+        bar = "———◉———————"
     elif 40 <= umm < 50:
-        bar = "————❥—————"
+        bar = "————◉——————"
     elif 50 <= umm < 60:
-        bar = "—————❥————"
+        bar = "—————◉————"
     elif 60 <= umm < 70:
-        bar = "——————❥———"
+        bar = "——————◉———"
     elif 70 <= umm < 80:
-        bar = "———————❥——"
+        bar = "———————◉——"
     elif 80 <= umm < 95:
-        bar = "————————❥—"
+        bar = "————————◉—"
     else:
-        bar = "—————————❥"
+        bar = "—————————◉"
     buttons = [
-         [
+        [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
         ],
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="▶️", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="⏸️", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="🔄", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="⏭️", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="⏹️", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/BRANDEDKING8"
+                text="🎵 Playlist", callback_data=f"show_playlist {chat_id}"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/BRANDED_WORLD"
+                text="⚙️ Settings", callback_data=f"show_settings {chat_id}"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -82,18 +82,18 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
 def stream_markup(_, videoid, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="▶️", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="⏸️", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="🔄", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="⏭️", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="⏹️", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/KotakBiasa"
+                text="🎵 Playlist", callback_data=f"show_playlist {chat_id}"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/DeltaStreamChat"
+                text="⚙️ Settings", callback_data=f"show_settings {chat_id}"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -201,26 +201,25 @@ def queue_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="II ᴘᴀᴜsᴇ",
+                text="⏸️ Jeda",
                 callback_data=f"ADMIN Pause|{chat_id}",
             ),
-            InlineKeyboardButton(text="▢ sᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="⏹️ Berhenti", callback_data=f"ADMIN Stop|{chat_id}"),
             InlineKeyboardButton(
-                text="sᴋɪᴘ ‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
+                text="⏭️ Lewati", callback_data=f"ADMIN Skip|{chat_id}"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="▷ ʀᴇsᴜᴍᴇ", callback_data=f"ADMIN Resume|{chat_id}"
+                text="▶️ Lanjutkan", callback_data=f"ADMIN Resume|{chat_id}"
             ),
             InlineKeyboardButton(
-                text="ʀᴇᴘʟᴀʏ ↺", callback_data=f"ADMIN Replay|{chat_id}"
+                text="🔄 Ulangi", callback_data=f"ADMIN Replay|{chat_id}"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴍᴏʀᴇ ❥",
-                callback_data=f"PanelMarkup None|{chat_id}",
+                text="⚙️ Lebih Banyak", callback_data=f"PanelMarkup None|{chat_id}",
             ),
         ],
     ]
@@ -237,21 +236,20 @@ def stream_markup2(_, chat_id):
             ),
         ],
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="▶️", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="⏸️", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="🔄", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="⏭️", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="⏹️", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/KotakBiasa"
+                text="🎵 Playlist", callback_data=f"show_playlist {chat_id}"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/DeltaStreamChat"
+                text="⚙️ Settings", callback_data=f"show_settings {chat_id}"
             ),
         ],
-
         [
             InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data="close"),
         ],
@@ -312,28 +310,28 @@ def panel_markup_1(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="🎧 sᴜғғʟᴇ ❥",
+                text="🔀 Acak",
                 callback_data=f"ADMIN Shuffle|{chat_id}",
             ),
-            InlineKeyboardButton(text="ʟᴏᴏᴘ ↺", callback_data=f"ADMIN Loop|{chat_id}"),
+            InlineKeyboardButton(text="Ulangi ↺", callback_data=f"ADMIN Loop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
-                text="◁ 10 sec",
+                text="⏪ 10 detik",
                 callback_data=f"ADMIN 1|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="10 sec ▷",
+                text="10 detik ⏩",
                 callback_data=f"ADMIN 2|{chat_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="❥ ʜᴏᴍᴇ ❥",
+                text="Beranda 🏠",
                 callback_data=f"Pages Back|2|{videoid}|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="❥ ɴᴇxᴛ ❥",
+                text="Berikutnya ➡️",
                 callback_data=f"Pages Forw|2|{videoid}|{chat_id}",
             ),
         ],
@@ -375,7 +373,7 @@ def panel_markup_2(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="❥ ʙᴀᴄᴋ ❥",
+                text="⬅️ Kembali",
                 callback_data=f"Pages Back|1|{videoid}|{chat_id}",
             ),
         ],
@@ -392,25 +390,25 @@ def panel_markup_5(_, videoid, chat_id):
             ),
         ],
         [
-            InlineKeyboardButton(text="ᴘᴀᴜsᴇ", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="sᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
-            InlineKeyboardButton(text="sᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="⏸️ Jeda", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="⏹️ Berhenti", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="⏭️ Lewati", callback_data=f"ADMIN Skip|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
-                text="Resume", callback_data=f"ADMIN Resume|{chat_id}"
+                text="▶️ Lanjutkan", callback_data=f"ADMIN Resume|{chat_id}"
             ),
             InlineKeyboardButton(
-                text="Replay", callback_data=f"ADMIN Replay|{chat_id}"
+                text="🔄 Ulangi", callback_data=f"ADMIN Replay|{chat_id}"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="❥ ʜᴏᴍᴇ ❥",
+                text="Beranda 🏠",
                 callback_data=f"MainMarkup {videoid}|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="❥ ɴᴇxᴛ ❥",
+                text="Berikutnya ➡️",
                 callback_data=f"Pages Forw|1|{videoid}|{chat_id}",
             ),
         ],
@@ -485,27 +483,27 @@ def panel_markup_4(_, vidid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="II ᴘᴀᴜsᴇ",
+                text="⏸️ Jeda",
                 callback_data=f"ADMIN Pause|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="▢ sᴛᴏᴘ ▢", callback_data=f"ADMIN Stop|{chat_id}"
+                text="⏹️ Berhenti", callback_data=f"ADMIN Stop|{chat_id}"
             ),
             InlineKeyboardButton(
-                text="sᴋɪᴘ ‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="▷ ʀᴇsᴜᴍᴇ", callback_data=f"ADMIN Resume|{chat_id}"
-            ),
-            InlineKeyboardButton(
-                text="ʀᴇᴘʟᴀʏ ↺", callback_data=f"ADMIN Replay|{chat_id}"
+                text="⏭️ Lewati", callback_data=f"ADMIN Skip|{chat_id}"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="❥ ʜᴏᴍᴇ ❥",
+                text="▶️ Lanjutkan", callback_data=f"ADMIN Resume|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="🔄 Ulangi", callback_data=f"ADMIN Replay|{chat_id}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="🏠 Beranda",
                 callback_data=f"MainMarkup {vidid}|{chat_id}",
             ),
         ],
@@ -523,23 +521,23 @@ def panel_markup_clone(_, vidid, chat_id):
             ),
         ],
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="▶️", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="⏸️", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="🔄", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="⏭️", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="⏹️", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
-                text="📥 ᴠɪᴅᴇᴏ", callback_data=f"downloadvideo {vidid}"
+                text="🎥 Video", callback_data=f"downloadvideo {vidid}"
             ),
             InlineKeyboardButton(
-                text="📥 ᴀᴜᴅɪᴏ", callback_data=f"downloadaudio {vidid}"
+                text="🔊 Audio", callback_data=f"downloadaudio {vidid}"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="✚ ᴘʟᴀʏʟɪsᴛ ✚", callback_data=f"branded_playlist {vidid}"
+                text="✚ Playlist ✚", callback_data=f"branded_playlist {vidid}"
             ),
         ],
     ]
--- DeltaMusic/utils/inlinequery.py
+++ DeltaMusic/utils/inlinequery.py
@@ -7,37 +7,37 @@ answer.extend(
         InlineQueryResultArticle(
             title="Pause",
             description="Jeda streaming yang sedang diputar di video chat.",
-            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            thumb_url="https://envs.sh/mtO.jpg",
             input_message_content=InputTextMessageContent("/pause"),
         ),
         InlineQueryResultArticle(
             title="Resume",
             description="Lanjutkan streaming yang dijeda di video chat.",
-            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            thumb_url="https://envs.sh/mtO.jpg",
             input_message_content=InputTextMessageContent("/resume"),
         ),
         InlineQueryResultArticle(
             title="Skip",
             description="Lewati streaming yang sedang diputar di video chat dan pindah ke streaming berikutnya.",
-            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            thumb_url="https://envs.sh/mtO.jpg",
             input_message_content=InputTextMessageContent("/skip"),
         ),
         InlineQueryResultArticle(
             title="End",
             description="Akhiri streaming yang sedang diputar di video chat.",
-            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            thumb_url="https://envs.sh/mtO.jpg",
             input_message_content=InputTextMessageContent("/end"),
         ),
         InlineQueryResultArticle(
             title="Shuffle",
             description="Acak lagu yang ada di daftar putar.",
-            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            thumb_url="https://envs.sh/mtO.jpg",
             input_message_content=InputTextMessageContent("/shuffle"),
         ),
         InlineQueryResultArticle(
             title="Loop",
             description="Ulangi trek yang sedang diputar di video chat.",
-            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            thumb_url="https://envs.sh/mtO.jpg",
             input_message_content=InputTextMessageContent("/loop 3"),
         ),
     ]
