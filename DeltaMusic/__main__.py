c2e1f5e0b24e12ce86d059d7a53b53836d9af61e
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-03-26 06:49:54 +0700

    update DeltaMusic/__main__.py

--- DeltaMusic/__main__.py
+++ DeltaMusic/__main__.py
@@ -60,3 +60,33 @@ async def init():
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())
+
+uvloop.install()
+
+async def job():
+    raise Exception("ouch")
+
+
+async def other_job():
+    try:
+        await asyncio.sleep(10)
+    except asyncio.CancelledError:
+        print("other_job was cancelled!")
+
+
+if __name__ == "__main__": 
+    aiorun.logger.disabled = True  
+    aiorun.run(init())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.create_task(job())
+    loop.create_task(other_job())
+
+    def handler(loop, context):
+        # https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.loop.call_exception_handler
+        print(f'Stopping loop due to error: {context["exception"]} ')
+        loop.stop()
+
+    loop.set_exception_handler(handler=handler)
+
+    run(loop=loop)
