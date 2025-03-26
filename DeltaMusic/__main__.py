0072f8ee38c3b7d209c8035b3a50a4b3d6eccca0
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-03-26 14:21:45 +0700

    update DeltaMusic/__main__.py

--- DeltaMusic/__main__.py
+++ DeltaMusic/__main__.py
@@ -32,7 +32,7 @@ async def init():
     await app.start()
 
     for module in ALL_MODULES:
-        importlib.import_module("DeltaMusic.plugins" + all_module)
+        importlib.import_module(f"DeltaMusic.plugins.{all_modules}")
 
     LOGGER("DeltaMusic.plugins").info("Berhasil mengimpor semua modul.")
 
