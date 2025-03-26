513b46ef7cec72e9954c4792c694cb4f6431c164
Opet321 <90963402+Opet321@users.noreply.github.com>
2025-03-26 14:59:58 +0700

    update DeltaMusic/plugins/__init__.py

--- DeltaMusic/plugins/__init__.py
+++ DeltaMusic/plugins/__init__.py
@@ -1,17 +1,23 @@
+
+
 import glob
-import os
+from os.path import dirname, isfile
+
 
 def __list_all_modules():
-    work_dir = os.path.dirname(__file__)
-    mod_paths = glob.glob(os.path.join(work_dir, "*.py"))  
+    work_dir = dirname(__file__)
+    mod_paths = glob.glob(work_dir + "/*/*.py")
 
     all_modules = [
-        os.path.basename(f)[:-3]  
+        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
         for f in mod_paths
-        if os.path.isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+        if isfile(f)
+        and f.endswith(".py")
+        and not f.endswith("__init__.py")
     ]
 
     return all_modules
+
 
 ALL_MODULES = sorted(__list_all_modules())
 __all__ = ALL_MODULES + ["ALL_MODULES"]
